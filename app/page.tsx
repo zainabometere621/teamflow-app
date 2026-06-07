abbebceac41d35a4793ba59d66f8546aacdcbedf
Zainab Ometere <zainabometere621@gmail.com>
2026-06-07 21:17:04 +0100

    feat: add auth feature folder and components structure

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1
-        className="font-bold text-4xl text-[#1e4db7]"
-      >
-        This is the TeamFlow
-      </h1>
+      <h1 className="font-bold text-4xl text-[#1e4db7]">This is TeamFlow</h1>
     </main>
   );
 }
