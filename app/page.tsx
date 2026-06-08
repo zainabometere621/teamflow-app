3583d3684280ffee40c8cb6ec3bb1456930971a7
Zainab Ometere <zainabometere621@gmail.com>
2026-06-08 16:04:31 +0100

    feat: build the landing.tsx uI component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Landing from "@/features/auth/components/Landing";
 
 export default function Home() {
   return (
     <main>
-      <h1 className="font-bold text-4xl text-[#1e4db7]">This is TeamFlow</h1>
+      <Landing/>
     </main>
   );
 }
--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,9 @@
 export const COLORS = {
-    primary: "#3b82f6",
+    primary: "#3b82f6 ",
     textPrimary: "#ffffff",
     textSecondary: "#94a3b8",
     inputBg: "#1e293b",
     inputBorder: "#2d3f55",
+    logoLightGardient: " #3b82f6",
+     logoBrightGardient: "#2563eb"
 }
