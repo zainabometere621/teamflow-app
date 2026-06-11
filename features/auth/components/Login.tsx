5cebcca378fb03caa3cfbf925b8f5516ec563d3a
Zainab Ometere <zainabometere621@gmail.com>
2026-06-11 10:59:40 +0100

    feat: build Register UI component

--- features/auth/components/Login.tsx
+++ features/auth/components/Login.tsx
@@ -11,12 +11,12 @@ import {
 
 export default function Login() {
   return (
-    <section className="space-y-4">
-      <div className="space-y-2">
+    <section className="space-y-6">
+      <div className="flex flex-col gap-2">
         <label className="text-[#ffffff] text-base">Email</label>
         <Input placeholder="you@gmail.com" className="placeholder-[#94a3b8]" />
       </div>
-      <div className="space-y-2">
+      <div className="flex flex-col gap-2">
         <label className="text-[#ffffff] text-base">Password</label>
         <Input
           type="password"
@@ -24,7 +24,7 @@ export default function Login() {
           className="placeholder-[#94a3b8]"
         />
       </div>
-      <div className="space-y-2">
+      <div className="flex flex-col gap-2">
         <label className="text-[#ffffff] text-base">Login as</label>
         <Select>
           <SelectTrigger className="bg-[#1e293b] border-[#2d3f55] text-[#ffffff]">
@@ -39,7 +39,7 @@ export default function Login() {
       </div>
       <div className="flex justify-between">
         <div className="flex items-center gap-2">
-         <Checkbox className="h-4 w-4 border border-[#94a3b8] bg-[#ffffff] data-[state=checked]:bg-[#3b82f6] data-[state=checked]:border-[#3b82f6]" />
+          <Checkbox className="h-4 w-4 border border-[#94a3b8] bg-[#ffffff] data-[state=checked]:bg-[#3b82f6] data-[state=checked]:border-[#3b82f6]" />
           <label className="text-base text-[#94a3b8]">Remember me</label>
         </div>
         <div>
--- features/auth/components/Register.tsx
+++ features/auth/components/Register.tsx
@@ -1,7 +1,48 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 export default function Register() {
-    return(
-        <section>
-            <h1 className="text-white">This is the register page</h1>
-        </section>
-    )
+  return (
+    <section className="space-y-6">
+      <div className="flex flex-col gap-2">
+        <label className="text-[#ffffff] text-base">Full Name</label>
+        <Input placeholder="John Doe" className="placeholder-[#94a3b8]" />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label className="text-[#ffffff] text-base">Email</label>
+        <Input placeholder="you@gmail.com" className="placeholder-[#94a3b8]" />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label className="text-[#ffffff] text-base">Password</label>
+        <Input
+          type="password"
+          placeholder="••••••••"
+          className="placeholder-[#94a3b8]"
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label className="text-[#ffffff] text-base">Role</label>
+        <Select>
+          <SelectTrigger className="bg-[#1e293b] border-[#2d3f55] text-[#ffffff]">
+            <SelectValue placeholder="Member" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="member">Member</SelectItem>
+            <SelectItem value="admin">Admin</SelectItem>
+            <SelectItem value="manager">manager</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+      <Button className="w-full bg-[#3b82f6] text-[#ffffff]">
+        Create account
+      </Button>
+    </section>
+  );
 }
